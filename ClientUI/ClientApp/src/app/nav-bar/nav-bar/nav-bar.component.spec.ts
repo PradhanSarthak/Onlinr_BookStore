487db2f93dbd9bbd788a34fbffa74b3e3bac367a
Sarthak Pradhan <psarthak1993@gmail.com>
2020-11-14 05:24:20 +0530

    Added ClientUI(Angular Project)

---
+++ ClientUI/ClientApp/src/app/nav-bar/nav-bar/nav-bar.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { NavBarComponent } from './nav-bar.component';
+
+let component: NavBarComponent;
+let fixture: ComponentFixture<NavBarComponent>;
+
+describe('nav-bar component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ NavBarComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(NavBarComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
