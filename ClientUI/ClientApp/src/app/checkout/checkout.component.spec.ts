467ffc439687ab110309c6aa40c11d02bc512ec6
Sarthak Pradhan <psarthak1993@gmail.com>
2020-11-14 12:37:07 +0530

    Client Busket

---
+++ ClientUI/ClientApp/src/app/checkout/checkout.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { CheckoutComponent } from './checkout.component';
+
+let component: CheckoutComponent;
+let fixture: ComponentFixture<CheckoutComponent>;
+
+describe('checkout component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ CheckoutComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(CheckoutComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ ClientUI/ClientApp/src/app/shared/components/order-totals/order-totals.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { OrderTotalsComponent } from './order-totals.component';
+
+let component: OrderTotalsComponent;
+let fixture: ComponentFixture<OrderTotalsComponent>;
+
+describe('order-totals component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ OrderTotalsComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(OrderTotalsComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
